76994a1f06967df73e166e6204ef9d9b2568b2c0
Thirdaii <greatglobber@gmail.com>
2026-08-31 01:11:37 -0500

    v1.7.28 wire WoW-style portraits into test build

--- build/v178_viewer_hotfix.py
+++ build/v178_viewer_hotfix.py
@@ -6,6 +6,7 @@ gear=here/'v178_wotlk_gear_patch.py'
 blend_probe=here/'v178_blend_probe_patch.py'
 additive_rescue=here/'v178_additive_rescue_patch.py'
 roster_drag=here/'v178_roster_drag_patch.py'
-for script in (core,gear,blend_probe,additive_rescue,roster_drag):
+wow_portraits=here/'v178_wow_portraits_patch.py'
+for script in (core,gear,blend_probe,additive_rescue,roster_drag,wow_portraits):
     ns={'__name__':'__main__','__file__':str(script)}
     exec(compile(script.read_text(encoding='utf-8'),str(script),'exec'),ns,ns)
