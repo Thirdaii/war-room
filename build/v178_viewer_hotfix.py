db365bd83cc75ce2b878fd846c214e5962105ce1
Thirdaii <greatglobber@gmail.com>
2026-09-01 07:43:21 -0500

    v1.7.28 wire v19-v21 UI integrations into test build

--- build/v178_viewer_hotfix.py
+++ build/v178_viewer_hotfix.py
@@ -10,6 +10,9 @@ wow_portraits=here/'v178_wow_portraits_patch.py'
 group_drag=here/'v178_group_drag_patch.py'
 group_drag_v17=here/'v178_group_drag_v17_patch.py'
 raidframe_v18=here/'v178_raidframe_polish_patch.py'
-for script in (core,gear,blend_probe,additive_rescue,roster_drag,wow_portraits,group_drag,group_drag_v17,raidframe_v18):
+raid_layout_v19=here/'v178_raid_layout_finish_v19_patch.py'
+smooth_drag_v20=here/'v178_smooth_group_drag_v20_patch.py'
+live_roster_v21=here/'v178_live_roster_v21_patch.py'
+for script in (core,gear,blend_probe,additive_rescue,roster_drag,wow_portraits,group_drag,group_drag_v17,raidframe_v18,raid_layout_v19,smooth_drag_v20,live_roster_v21):
     ns={'__name__':'__main__','__file__':str(script)}
     exec(compile(script.read_text(encoding='utf-8'),str(script),'exec'),ns,ns)
