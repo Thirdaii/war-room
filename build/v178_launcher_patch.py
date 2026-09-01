ed14263d4eab203cd35069832e7eb6249fb742ca
Thirdaii <greatglobber@gmail.com>
2026-09-01 10:17:06 -0500

    v1.7.28 wire V31.3 default window sizing

--- build/v178_launcher_patch.py
+++ build/v178_launcher_patch.py
@@ -7,6 +7,7 @@ live_roster=here/'v178_live_roster_launcher_patch.py'
 live_roster_v22=here/'v178_live_roster_launcher_v22_patch.py'
 live_roster_v24=here/'v178_live_roster_launcher_v24_patch.py'
 live_roster_v25=here/'v178_live_roster_launcher_v25_patch.py'
-for script in (core,extra,live_roster,live_roster_v22,live_roster_v24,live_roster_v25):
+window_v313=here/'v178_window_v313_patch.py'
+for script in (core,extra,live_roster,live_roster_v22,live_roster_v24,live_roster_v25,window_v313):
     ns={'__name__':'__main__','__file__':str(script)}
     exec(compile(script.read_text(encoding='utf-8'),str(script),'exec'),ns,ns)
